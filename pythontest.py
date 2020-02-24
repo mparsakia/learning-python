d82afba7bd79133cc98a2d4c7f8d3d332c28d5fb
Mike <mparsakia@users.noreply.github.com>
2020-02-24 02:01:35 -0800

    learn dictionary, list, and other functions

--- pythontest.py
+++ pythontest.py
@@ -119,10 +119,10 @@ input("Press enter to continue.")
 
 
 # Basic while loop example
-number = 1
-while number < 7:
-	print(number, end=" ")
-	number = number + 1
+numbera = 1
+while numbera < 7:
+	print(numbera, end=" ")
+	numbera = numbera + 1
 input("Press enter to continue.")
 
 
@@ -216,6 +216,118 @@ print(loop(1,5,0)) # Should be 1 2 3 4
 print(loop(-1,-2,0)) # Should be -1
 print(loop(10,25,-2)) # Should be 10 12 14 16 18 20 22 24 
 print(loop(1,1,1)) # Should be empty
+input("Press enter to continue.")
+
+
+
+# The format_address function separates out parts of the address string into new strings: 
+# house_number and street_name, and returns: "house number X on street named Y". The format 
+# of the input string is: numeric house number, followed by the street name which may contain numbers,
+#  but never by themselves, and could be several words long. For example, "123 Main Street", "1001 1st Ave", or "55 North Center Drive".
+def format_address(address_string):
+  address = address_string          # Maybe not needed but I dont know if param is const (coming from c++ knowledge)
+  sublist = address.split(' ')      # Split the string delim by space, returns list
+  housenum = sublist[0]             # The house number (list format)
+  housenum = str(housenum)          # Converted to string (verify: print(type(housenum)) Could have chained above, this is clearer for example.
+  streetlist = sublist[1:]          # Streetnames, list format still
+  streetname = ' '.join([str(x) for x in streetlist])
+                                    #line above takes all in sublist[1:] and makes it one long string, adds " " in between the list items
+  return f"house number {housenum} on street named {streetname}"
+                                    #Using String Literals / Template Literals with 'f' before string.
+  
+print(format_address("123 Main Street")) # Should print: "house number 123 on street named Main Street"
+print(format_address("1001 1st Ave"))
+print(format_address("55 North Center Drive"))
+input("Press enter to continue.")
+
+
+
+# The highlight_word function changes the given word in a sentence to its upper-case version. 
+# For example, highlight_word("Have a nice day", "nice") returns "Have a NICE day". 
+# Can you write this function in just one line?
+def highlight_word(sentence, word):
+	return(sentence.replace(word,word.upper()))
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
+input("Press enter to continue.")
+
+
+
+# Combine two lists. One list is in reverse order, 'reverse' it to make it correct.
+def combine_lists(list1, list2):
+  newlist = []
+  newlist.extend(list1)
+  list2.reverse()      
+  newlist.extend(list2)
+  return newlist
+  
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+print(combine_lists(Jamies_list, Drews_list))
+input("Press enter to continue.")  # prints ['Alice', 'Cindy', 'Bobby', 'Jan', 'Peter', 'Marcia', 'Greg', 'Carol', 'Mike']
+
+# Use a list comprehension to create a list of squared numbers (n*n). 
+# The function receives the variables start and end, and returns a list of squares of consecutive numbers 
+# between start and end inclusively. For example, squares(2, 3) should return [4, 9].
+# Very Helpful Note: https://www.datacamp.com/community/tutorials/python-list-comprehension listcomps are kind of like lambdas in c++
+def squares(start, end):
+	return [x**2 for x in range(start,end+1)]
+
+print(squares(2, 3)) # Should be [4, 9]
+print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# iterate through the keys and values of the car_prices dictionary, printing out some information about each one.
+def car_listing(car_prices):
+  result = ""
+  for key, value in car_prices.items():
+    result += str(key) + " costs " + str(value) + " dollars\n"
+  return result
+  
+print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000})) # Prints Kia Soul costs 19000 dollars ... so on
+input("Press enter to continue.")
+
+
+
+# Taylor and Rory are hosting a party. They sent out invitations, and each one collected responses into dictionaries, 
+# with names of their friends and how many guests each friend is bringing. Each dictionary is a partial list, 
+# but Rory's list has more current information about the number of guests. Fill in the blanks to combine both dictionaries 
+# into one, with each friend listed only once, and the number of guests from Rory's dictionary taking precedence,
+# if a name is included in both dictionaries. Then print the resulting dictionary.
+def combine_guests(guests1, guests2):
+  # Combine both dictionaries into one, with each key listed 
+  # only once, and the value from guests1 taking precedence
+  g1 = guests1
+  g2 = guests2
+  g2.update(g1)     #dict1.update(dict2)great function, merges the lists and replaces key:value's of dict1 with dict2 
+  return g2
+
+Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
+Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
+print(combine_guests(Rorys_guests, Taylors_guests))
+input("Press enter to continue.") #{'David': 1, 'Nancy': 1, 'Robert': 4, 'Adam': 2, 'Samantha': 3, 'Chris': 5, 'Brenda': 3, 'Jose': 3, 'Charlotte': 2, 'Terry': 1}
+
+
+
+# Use a dictionary to count the frequency of letters in the input string. Only letters should be counted, not blank spaces, 
+# numbers, or punctuation. Upper case should be considered the same as lower case. 
+# For example, count_letters("This is a sentence.") should return 
+# {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}.
+from collections import Counter
+
+def count_letters(text):
+  return dict(Counter((''.join(c for c in text if c.isalpha()).lower())))
+    # What a godline. Let's explain:
+    # dict will return a dictionary as requested in the problem
+    # Counter is an object/class that will do what it says, count the items and put them in a dictionary, but it returns "Counter({...})"
+    # ''.join(c for c in text if c.isalpha()) will strip whitespace and add to a new string if the value "is alpha"
+    # remember that c for c in text will iterate through the string and then the .isalpha() only adds it to the new string, if its alphabet only
+    # And .lower() will force that entire string lowercase. Remember functions always work from the inside out, so follow the parenthesis!
+print(count_letters("AaBbCc")) # Should be {'a': 2, 'b': 2, 'c': 2}
+print(count_letters("Math is fun! 2+2=4")) # Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+print(count_letters("This is a sentence.")) # Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
 input("Press enter to continue.")
 
 
